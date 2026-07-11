14a3c06ff20963edeeb825f439238b448fd655bf
Bappadala Rohith Kumar Naidu <198095685+rohitkumarnaidu@users.noreply.github.com>
2026-07-11 17:50:27 +0530

    docs(runbooks): add ssl-renewal documentation

---
+++ packages/config/eslint-preset.js
@@ -0,0 +1,62 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'separate-type-imports',
+      },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react-hooks/exhaustive-deps': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'eqeqeq': ['error', 'smart'],
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**', '**/*.test.*', '**/*.spec.*'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+};
